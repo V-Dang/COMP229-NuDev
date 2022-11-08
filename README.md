5721cfca781a7e6f9685345b3af307e257c4bf49
V-Dang <vivian.dang96@hotmail.com>
2022-11-08 16:17:16 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# COMP229-Assignment-2-Vivian-Dang-302278335
---
+++ server/config/db.js
@@ -0,0 +1,9 @@
+//COMP229-Assignment-2-Vivian-Dang-302278335
+
+//Must decalre URI, uses book_store database from local
+module.exports = {
+    //URI: "mongodb://127.0.0.1:27017/assignment2"         //numbers found on listening terminal mongod
+    "URI": "mongodb+srv://user1:LryS3WGEtxzjtFN9@cluster0.dk2giiv.mongodb.net/assignment2"
+    //user:user1
+    //pw:LryS3WGEtxzjtFN9
+};
