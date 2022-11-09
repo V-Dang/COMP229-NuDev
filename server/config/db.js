bfe98414b6f6a58e32765d2676bbb2c949d6b9fc
a70ssein <113375740+a70ssein@users.noreply.github.com>
2022-11-08 20:31:31 -0500

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -4,6 +4,7 @@
 module.exports = {
     //URI: "mongodb://127.0.0.1:27017/assignment2"         //numbers found on listening terminal mongod
     "URI": "mongodb+srv://user1:LryS3WGEtxzjtFN9@cluster0.dk2giiv.mongodb.net/assignment2"
+        //"URI": "mongodb+srv://user1:LryS3WGEtxzjtFN9@cluster0.dk2giiv.mongodb.net/survay"
     //user:user1
     //pw:LryS3WGEtxzjtFN9
 };
