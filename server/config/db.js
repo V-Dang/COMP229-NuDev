307fdb271fb5cd6ea53e5c02e744a6262bc5dfe9
V-Dang <vivian.dang96@hotmail.com>
2022-11-11 19:48:04 -0500

    fixed contacts page

--- server/config/db.js
+++ server/config/db.js
@@ -3,9 +3,9 @@
 //Must decalre URI
 module.exports = {
         //numbers found on listening terminal mongod
-    //"URI": "mongodb+srv://user1:LryS3WGEtxzjtFN9@cluster0.dk2giiv.mongodb.net/assignment2"
+    "URI": "mongodb+srv://user1:LryS3WGEtxzjtFN9@cluster0.dk2giiv.mongodb.net/assignment2"
       
-    "URI":"mongodb+srv://Hussein:dLqouWQ7F8sg4vkq@dbserver.atdtgw9.mongodb.net/survey"
+    //"URI":"mongodb+srv://Hussein:dLqouWQ7F8sg4vkq@dbserver.atdtgw9.mongodb.net/survey"
    
     //user:user1
     //pw:LryS3WGEtxzjtFN9
